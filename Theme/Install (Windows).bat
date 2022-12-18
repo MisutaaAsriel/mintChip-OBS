a3e703cd646aa3351ca4b593777184d7246a723f
Misutaa Asriel <8140068+MisutaaAsriel@users.noreply.github.com>
2022-12-18 16:33:29 -0600

    Added platform install scripts
    Added platform install scripts for Windows & Mac + Linux (POSIX?)

---
+++ Theme/Install (Windows).bat
@@ -0,0 +1,17 @@
+@echo off
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+echo Checking for theme directory...
+mkdir "%APPDATA%\obs-studio\themes\"
+
+echo.
+echo Copying theme...
+COPY /V /Y "%CD%\mintChip.qss" "%APPDATA%\obs-studio\themes\"
+echo.
+echo Copying Assets...
+XCOPY /E /I /F /Y "%CD%\Mintchip" "%APPDATA%\obs-studio\themes\Mintchip"
+
+echo.
+echo Press any key to exit...
+PAUSE > NUL
